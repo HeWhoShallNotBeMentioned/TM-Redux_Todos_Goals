e0cea6bbd78bb289e5fa1ab1428817932ba216f0
Chris Underwood <cunderwoodmn@gmail.com>
2018-12-18 19:40:36 -0800

    put rest of todo actions in our todos reducer and converted from if/else statements to switch/case

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
+// {
+//  type: "REMOVE_TODO",
+//  id: 0
+// }
 
-{
- type: "REMOVE_TODO",
- id: 0
+// {
+//   type: "TOGGLE_TODO",
+//   id: 0
+//  }
+
+//  {
+//    type: 'ADD_GOAL',
+//    goal: {
+//      id: 0,
+//      name: 'Run a marathon',
+//    }
+//  }
+
+//  {
+//    type: 'REMOVE_GOAL',
+//    id: 0
+//  }
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.todo]);
+    case 'REMOVE_TODO':
+      return state.filter(todo => {
+        return todo.id !== action.id;
+      });
+    case 'TOGGLE_TODO':
+      return state.map(todo =>
+        (todo.id !== action.id
+          ? todo
+          : Object.assign({}, todo, { complete: !todo.complete })));
+    default:
+      return state;
+  }
 }
-
-{
-  type: "TOGGLE_TODO",
-  id: 0
- }
-
- {
-   type: 'ADD_GOAL',
-   goal: {
-     id: 0,
-     name: 'Run a marathon',
-   }
- }
-
- {
-   type: 'REMOVE_GOAL',
-   id: 0
- }
-
- function todos (state = [], action) {
-   if (action.type === 'ADD_TODO'){
-     return state.concat([action.todo])
-   }
-
-   return state
- }
 
 function createStore(reducer) {
   //4 parts of the store
@@ -48,25 +57,27 @@ function createStore(reducer) {
     };
   };
 
-  const dispatch = (action) => {
+  const dispatch = action => {
     state = reducer(state, action);
-    listeners.forEach((listener) => listener())
-
-  }
+    listeners.forEach(listener => listener());
+  };
 
   return {
     getState,
     subscribe,
-    dispatch
+    dispatch,
   };
 }
 
 const store = createStore(todos);
+store.subscribe(() => {
+  console.log('the new state is: ', store.getState());
+});
 store.dispatch({
   type: 'ADD_TODO',
   todo: {
-    id:0,
+    id: 0,
     name: 'Learn Redux',
-    complete: false
-  }
-})
+    complete: false,
+  },
+});
