469112288a0bae6e1f116be8a844f147d37e1158
Chris Underwood <cunderwoodmn@gmail.com>
2018-12-20 15:57:16 -0800

    add functionality to add todos via react.

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Todos />
+        <Todos store={this.props.store} />
         <Goals />
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App store={store} />, document.getElementById('app'));
--- todos.js
+++ todos.js
@@ -1,8 +1,28 @@
 class Todos extends React.Component {
+  addItem = e => {
+    e.preventDefault();
+    const name = this.input.value;
+    this.input.value = '';
+
+    this.props.store.dispatch(
+      addTodoAction({
+        id: generateId(),
+        name,
+        complete: false,
+      })
+    );
+  };
   render() {
+    // refs is react feature for getting data from an uncontrolled component
     return (
       <div>
-        TODOS
+        <h1>Todo List</h1>
+        <input
+          type="text"
+          placeholder="Add Todo"
+          ref={input => (this.input = input)}
+        />
+        <button onClick={this.addItem}>Add Todo</button>
         <List />
       </div>
     );
