8f41b587151f163bc07e45ad57dc08d6dbb1e319
Chris Underwood <cunderwoodmn@gmail.com>
2018-12-19 19:13:27 -0800

    implement custom middleware to learn about redux middleware

--- index.js
+++ index.js
@@ -81,6 +81,22 @@ function removeGoalAction(goalId) {
   };
 }
 
+const checker = store => next => action => {
+  if (
+    action.type === ADD_TODO &&
+    action.todo.name.toLowerCase().indexOf('bitcoin') !== -1
+  ) {
+    return alert("Nope. That's a bad idea.");
+  }
+  if (
+    action.type === ADD_GOAL &&
+    action.goal.name.toLowerCase().indexOf('bitcoin') !== -1
+  ) {
+    return alert("Nope. That's a bad idea.");
+  }
+  return next(action);
+};
+
 function todos(state = [], action) {
   switch (action.type) {
     case ADD_TODO:
@@ -119,7 +135,14 @@ function goals(state = [], action) {
 //   };
 // }
 
-const store = Redux.createStore(Redux.combineReducers({ todos, goals }));
+const store = Redux.createStore(
+  Redux.combineReducers({
+    todos,
+    goals,
+  }),
+  Redux.applyMiddleware(checker)
+);
+
 store.subscribe(() => {
   const { goals, todos } = store.getState();
 
